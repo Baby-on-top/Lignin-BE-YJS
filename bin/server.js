e5ccc8aec89255c2a493977dd423cbe2426f0d34
chwangmin <ckm7907cb@gmail.com>
2023-08-04 17:18:32 +0900

    :memo: Modify localhost -> ec2 server

--- bin/server.js
+++ bin/server.js
@@ -3,33 +3,35 @@
 /**
  * @type {any}
  */
-const WebSocket = require('ws')
-const http = require('http')
-const wss = new WebSocket.Server({ noServer: true })
-const setupWSConnection = require('./utils.js').setupWSConnection
+const WebSocket = require("ws");
+const http = require("http");
+const wss = new WebSocket.Server({ noServer: true });
+const setupWSConnection = require("./utils.js").setupWSConnection;
 
-const host = process.env.HOST || 'localhost'
-const port = process.env.PORT || 1234
+const host =
+  process.env.HOST ||
+  "http://ec2-3-35-208-69.ap-northeast-2.compute.amazonaws.com/";
+const port = process.env.PORT || 1234;
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('okay')
-})
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("okay");
+});
 
-wss.on('connection', setupWSConnection)
+wss.on("connection", setupWSConnection);
 
-server.on('upgrade', (request, socket, head) => {
+server.on("upgrade", (request, socket, head) => {
   // You may check auth of request here..
   // See https://github.com/websockets/ws#client-authentication
   /**
    * @param {any} ws
    */
-  const handleAuth = ws => {
-    wss.emit('connection', ws, request)
-  }
-  wss.handleUpgrade(request, socket, head, handleAuth)
-})
+  const handleAuth = (ws) => {
+    wss.emit("connection", ws, request);
+  };
+  wss.handleUpgrade(request, socket, head, handleAuth);
+});
 
 server.listen(port, host, () => {
-  console.log(`running at '${host}' on port ${port}`)
-})
+  console.log(`running at '${host}' on port ${port}`);
+});
